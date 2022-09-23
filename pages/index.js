bdc71da0eaebb33248b3c75dab15d5665c697b2a
nightmarelullaby <videotuto32@gmail.com>
2022-09-23 14:22:35 -0400

    a ver

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <>
 
       <div style={{height:"100vh",display:"flex",alignItems:"center",flexDirection:"column",justifyContent:"center"}}>
-        <h1 className={styles.homeTitle}>Bienvenido</h1>
+        <h1 className={styles.homeTitle}>Bienvenido1</h1>
         <p className={styles.homeDesc}>Diviértete explorando el catálogo</p>
           <Button href="/AllGames?page=1" as="/AllGames?page=1">
             Ir allí
