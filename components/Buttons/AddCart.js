c9e4fce9b099176479d43b0a991a097214950e0d
nightmarelullaby <videotuto32@gmail.com>
2023-05-02 03:28:56 -0400

    svg's fixed

--- components/Buttons/AddCart.js
+++ components/Buttons/AddCart.js
@@ -16,9 +16,21 @@ const AddCart = ({
                         </>
 
     const secondState = <>
-                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4.5 12.75L10.5 18.75L19.5 5.25" stroke="var(--blue)" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
+                                <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="var(--blue)"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.8"
+        d="M4.5 12.75l6 6 9-13.5"
+      ></path>
+    </svg>
 
             <small style={{color:"#0f1214",fontWeight:400}}>{second}</small>      
     </>
